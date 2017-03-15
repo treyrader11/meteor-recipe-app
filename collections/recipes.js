ec33edadde8e8f321c09b4524c0f32f3851e669b
Trey Rader <treyrdr09@gmail.com>
2017-03-15 17:44:14 -0500

    upgraded pkg and modified recipe.js

--- collections/recipes.js
+++ collections/recipes.js
@@ -1,4 +1,4 @@
-Recipes = new Meteor.Collection('recipes');
+/*Recipes = new Mongo.Collection('recipes');
 
 RecipeSchema = new SimpleSchema({
 	name: {
@@ -24,5 +24,44 @@ RecipeSchema = new SimpleSchema({
 		}
 	}
 })
+
+Recipes.attachSchema(RecipeSchema);
+*/
+
+import SimpleSchema from 'simpl-schema';
+SimpleSchema.extendOptions(['autoform']);
+
+Recipes = new Mongo.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+    name: {
+        label: "Name",
+        type: String
+    },
+    desc: {
+        label: "Description",
+        type: String
+    },
+    author: {
+        type: String,
+        label: "Author",
+        autoform: {
+            type: "hidden"
+        },
+        autoValue: function(){
+            return this.userId;
+        },
+    },
+    createdAt: {
+        type: Date,
+        label: "CreatedAt",
+        autoform: {
+            type: "hidden"
+        },
+        autoValue: function() {
+            return new Date();
+        },
+    }
+});
 
 Recipes.attachSchema(RecipeSchema);
